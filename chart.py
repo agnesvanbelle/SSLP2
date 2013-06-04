8e96ad8f8a6c1bae6b7ab8f4d90d31bf3c2af5f6
Agnes van Belle <agnesvanbelle@gmail.com>
2013-06-04 16:56:24 +0200

    fghg

--- chart.py
+++ chart.py
@@ -9,6 +9,9 @@ import re
 normal_brackets = '[]'
 inv_brackets = '<>'
 
+
+def make_itg():
+	pass
 # put the initial tree nodes in a CYK-like chart
 def make_syntax_chart(sentence_tree,  syntax_chart, col=0, row=None) :
 
@@ -69,9 +72,10 @@ def make_parse_chart(sentence_tree, reorderings_list) :
   # multimap by virtue of using lists as values
   parse_chart = defaultdict(list)
 
-  for key, value in syntax_chart.iteritems():      
-    parse_chart[key].append(syntax_chart.get(key))
-    
+#  for key, value in syntax_chart.iteritems():      
+ #   parse_chart[key].append(syntax_chart.get(key))
+  for i in range(0, len_sentence):
+    parse_chart[(i,i)] = [(syntax_chart[(i,i)],( 0, 0, 0))]
   print "parse chart:"
   printdict(parse_chart)
 
@@ -101,41 +105,37 @@ def make_parse_chart(sentence_tree, reorderings_list) :
           for l in range(0, len(leftkids)):
             for r in range(0, len(rightkids)):
               print "leftkid: %s, rightkid: %s \n" % (leftkids[l], rightkids[r])
-              parent_rule = get_parent_syntax_chart(parse_chart, len_sentence, span, leftkids[l], rightkids[r])
+              parent_rule = get_parent_syntax_chart(syntax_chart,parse_chart, len_sentence, span, leftkids[l], rightkids[r])
               
               if not not parent_rule:
                 print "parent_rule: %s" % parent_rule
-                if parent_rule not in parse_chart[span]:
-                  parse_chart[span].append( parent_rule +  parent_extension)
-                  print "added %s at place %s" % (parent_rule, span)
-                  print "parse chart there now %s\n" % parse_chart[span]
+                #if parent_rule[0] not in parse_chart[span]:
+		parse_chart[span].extend( [(parent_rule[0] +  parent_extension, (split_right[0], l, r))])
+		print "added %s at place %s" % (parent_rule, span)
+		print "parse chart there now %s\n" % parse_chart[span]
               
-          # get leftkids
-          # get rightkids, from parse chart
-
-          # for all combi's of them
-            # get parent in syntax chart (create on if it isn't there)
-            # check for inversion of left and right kid acc. to reorderings
 
       else :
         print "span %s is not a valid phrase " % (span,)
   
   printdict(parse_chart)
+  return parse_chart
 
-def get_parent_syntax_chart(parse_chart,  len_sentence, parent_span, leftkid, rightkid):
+def get_parent_syntax_chart(syntax_chart, parse_chart,  len_sentence, parent_span, leftkid, rightkid):
 
   max_combined_rules = 1
   
-  if not not parse_chart[parent_span]:
-    return parse_chart[parent_span][0]
+  #if not not parse_chart[parent_span]:
+  if not not syntax_chart.get(parent_span, ""):
+    return [syntax_chart[parent_span]]
   else :
     # check for rule
     regex = "r[\\/+]+"
 
     #filter(lambda l:  l and len(l[0])<2, map(lambda a : re.findall(r"[\\/+]+",  a), s))
-    if not too_many_combinations(leftkid, rightkid, max_combined_rules):
+    if not too_many_combinations(leftkid[0], rightkid[0], max_combined_rules):
     #if not any (re.search(regex,  kid) for kid in [leftkid,rightkid]):
-      return "(" + leftkid + "+" + rightkid + ")"
+      return ["(" + leftkid[0] + "+" + rightkid[0] + ")"]
       
     else :
       # missing right part
@@ -143,17 +143,19 @@ def get_parent_syntax_chart(parse_chart,  len_sentence, parent_span, leftkid, ri
       while  row < len_sentence-1 :
         row += 1
         print "checking right missing for %s" % ((parent_span[0], row),)
-        left_super = parse_chart[(parent_span[0], row)]
+        left_super = syntax_chart[(parent_span[0], row)]
         print "left_super: %s" % left_super
         if not not left_super: # if not empty cell          
           right_missing_row = row 
           right_missing_col = parent_span[1]+1
           right_missing = parse_chart[(right_missing_col, right_missing_row)]
           print "right_missing: %s at %s" % (right_missing, (right_missing_col, right_missing_row))
-
-          if not not right_missing :
-            if not too_many_combinations(left_super[0], right_missing[0], max_combined_rules):
-              return "(" + left_super[0] + "/" + right_missing[0] + ")"
+	  
+	  result = []
+	  for r in range(0, len(right_missing)):
+	    if not too_many_combinations(left_super[0][0], right_missing[r][0], max_combined_rules):
+	      result.extend( ["(" + left_super[0][0] + "/" + right_missing[r][0] + ")"])
+	  return result
     
                       
       #  missing left part
@@ -161,20 +163,55 @@ def get_parent_syntax_chart(parse_chart,  len_sentence, parent_span, leftkid, ri
       while col > 0 :
         col -= 1
         print "checking left missing for %s" % ((col, parent_span[1]),)
-        right_super = parse_chart[(col, parent_span[1])]
+        right_super = syntax_chart[(col, parent_span[1])]
         print "right_super: %s" % right_super
         if not not right_super: # if not empty cell          
           left_missing_row = parent_span[0]-1 
           left_missing_col = col
           left_missing = parse_chart[(left_missing_col, left_missing_row)]
           print "left_missing: %s at %s" % (left_missing, (left_missing_col, left_missing_row))
-          if not not left_missing:
-            if not too_many_combinations(right_super[0], left_missing[0], max_combined_rules):
-              return "(" + right_super[0] + "\\" + left_missing[0] + ")"
+          
+	  result = []
+	  for l in range(0, len(left_missing)):
+	    if not too_many_combinations(right_super[0][0], left_missing[l][0], max_combined_rules):
+	      result.extend( ["(" + right_super[0][0] + "\\" + left_missing[l][0] + ")"])
+	  return result
               
-    return ""
-    pass
+  return ""
 
+
+def get_rules_from_parse_chart(parse_chart, sentence_tree):
+
+  sentence_list = sentence_tree.leaves()
+  len_sentence = len(sentence_list)
+  binary_rules = [] 
+  unary_rules = []
+
+  for span_size in range(len_sentence-1, -1, -1):
+	  for span_startpos in range(0, len_sentence-span_size):
+		  span_endpos = span_startpos+span_size
+		  span = (span_startpos, span_endpos)
+		  print " ==> span_size: %d \n span: %s " % (span_size, span)
+				  
+		  parents = parse_chart[(span)]
+		  for p in range(0, len(parents)):
+		    parent = parents[p]
+		    LHS = parent[0]
+		    if parent[1][0]-1 >= 0: 
+		      RHS1_loc = (span_startpos, parent[1][0]-1)
+		      RHS2_loc = (parent[1][0], span_endpos)
+		      RHS1 = parse_chart[RHS1_loc][parent[1][1]][0]
+		      RHS2 = parse_chart[RHS2_loc][parent[1][2]][0]
+		      binary_rules.append((LHS,(RHS1,RHS2)))
+		      print (LHS,(RHS1,RHS2))
+		    else:
+		      unary_rules.append((LHS,sentence_list[span_startpos]))
+		      print (LHS,sentence_list[span_startpos])
+		      
+#	for i in range(0, len(binary_rules)):
+#	  print binary_rules[i]
+
+  return [binary_rules,unary_rules]
 
 def too_many_combinations(leftpart, rightpart, max_combined_rules):
   return len(filter(lambda l:  not l or len(l[0])<max_combined_rules, map(lambda a : re.findall(r"[\\/+]+",  a), [leftpart,rightpart]))) < 2
@@ -186,19 +223,19 @@ def is_inverted(leftspan, rightspan, reorderings_list):
   return reorderings_list[leftspan[1]] > reorderings_list[rightspan[0]]
 
 def test() :
-  #sentence_tree = Tree('(S (NP (N man)) (VP (V bites) (NP (N dog))))')
-  #reorderings_list = [2, 1, 0]
+  sentence_tree = Tree('(S (NP (N man)) (VP (V bites) (NP (N dog))))')
+  reorderings_list = [2, 1, 0]
    
   #sentence_tree = Tree('(S (ADVP (RB next)) (, ,) (NP (PRP we)) (VP (VBP have) (NP (NP (NNS flags)) (PP (IN of) (NP (NN convenience))))) (. .)) ')
   #reorderings_list = [0, 1, 2, 3, 4, 5, 6, 7]
 
   
-  sentence_tree = Tree('(A (G (H h) (I i) (J j)) (B (C (D d) (E e)) (F f)))')
-  reorderings_list = [0,1,2,3,4,5]
+  #sentence_tree = Tree('(A (G (H h) (I i) (J j)) (B (C (D d) (E e)) (F f)))')
+  #reorderings_list = [0,1,2,3,4,5]
   
   
   #sentence_tree = Tree('(S (A a) (B b ) (C c) (D d))')
-  #reorderings_list = [0,1,3,2]
+  #reorderings_list = [0,1,2,3]
   
   #treetransforms.chomsky_normal_form(sentence_tree, factor='right', horzMarkov=None, vertMarkov=0, childChar='|', parentChar='^')
 
@@ -207,14 +244,13 @@ def test() :
 
   #print list(enumerate(sentence_tree))
 
-  syntax_chart = {}
-
   #print sentence_tree[0]
 
 
 
-  make_parse_chart(sentence_tree, reorderings_list)
-
+  pc = make_parse_chart(sentence_tree, reorderings_list)
+	
+  [binary_rules, unary_rules] = get_rules_from_parse_chart(pc, sentence_tree)
   #sentence_tree.draw()
 
 
